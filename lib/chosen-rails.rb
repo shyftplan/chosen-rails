019c3e14f4b3c6495a3a887e189cb95b4dc47a03
Slava Korolev <korolvs@users.noreply.github.com>
2018-02-27 15:28:05 +0100

    Revert "Trying to update (#1)"
    This reverts commit bfedc295d18e1bfd3d322393b584f7a85fd63560.

--- lib/chosen-rails.rb
+++ lib/chosen-rails.rb
@@ -6,10 +6,12 @@ module Chosen
 end
 
 case ::Rails.version.to_s
-when /^(4|5)/
+when /^4/
   require 'chosen-rails/engine'
 when /^3\.[12]/
   require 'chosen-rails/engine3'
 when /^3\.[0]/
   require 'chosen-rails/railtie'
 end
+
+require 'compass-rails'
